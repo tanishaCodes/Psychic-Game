90f6949d24644c3b6eafc3796cf7379cc0792bfe
Tanisha Howell <thowell@protonmail.com>
2020-01-03 01:47:16 -0500

    finished README, javascript and index

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Psychic-Game
+Can you guess what letter computer is thinking? Try your luck and press a letterkey. 
+
+If you guess the letter the computer is thinking, you win!
+
+If you don't guess correctly by the end of your 9 tries, you will lose, but have better luck nextime!
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//naming all of the variables
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+var userGuess = "";
+var computerGuess = "";
+var lettersGuessed = "";
+var resetGame;
+
+function computerGuess() {
+var letter = letters[Math.floor(Math.random() * letters.length) * 26];
+}
+
+function updatelettersGuessed() {
+document.querySelector("#lettersGuessed").innerHTML = lettersGuessed.join(", ");
+}
+
+//user's guess 
+var userGuess = document.getElementById("letters");
+    
+/*when the user presses a letter key, a comparason is run. The user's guess is compared to the computer's guess. Then, tallies the wins, losses, and letters guessed.*/
+document.onkeydown = function(event)  {
+var userGuess = event.key;
+}
+
+    if (userGuess === computerGuess) {
+        alert("Congratulations! You've won that one!");
+        wins++;
+    }
+
+    while (userGuess != "computerGuess") {
+        guessesLeft--;
+    }
+
+//After no guesses are left, the game ends and resets.
+    if (guessesLeft == 0) {
+        document.reset() = "Bloop! Out of Guesses! I'll reset the game.";
+        losses--;
+    }
+
+    else if (!letters) {
+        alert("That is not a letter -___-");
+     }
